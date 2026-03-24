582add59992c6d53d2de6d3cb173af699ee2924b
Vera Marie Therese Abayan Orbello <veramarie@Veras-MacBook-Air.local>
2026-03-24 22:43:06 +0800

    Add CampusBuddy project to Part2

--- Part2/CampusBuddy/app/build.gradle.kts
+++ Part2/CampusBuddy/app/build.gradle.kts
@@ -1,23 +1,13 @@
-plugins {
-    alias(libs.plugins.android.application)
-}
-
 android {
-    namespace = "com.example.campusbuddy"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    namespace = "com.example.studybuddy"  // Your package name may differ
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.campusbuddy"
-        minSdk = 27
-        targetSdk = 36
+        applicationId = "com.example.studybuddy"
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -29,18 +19,14 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
 
-dependencies {
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
