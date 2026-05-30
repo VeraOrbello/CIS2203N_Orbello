36da8f34718832c3ab8686fd023852e03eee2dc7
Vera Marie Therese Abayan Orbello <YOUR_GITHUB_EMAIL>
2026-05-30 12:52:55 +0800

    Task F1

--- Integrated/EchoApp/app/src/main/java/com/example/echoapp/CalculatorActivity.kt
+++ Integrated/EchoApp/app/src/main/java/com/example/echoapp/CalculatorActivity.kt
@@ -2,7 +2,17 @@ package com.example.echoapp
 
 class CalculatorActivity {
 
-    fun addNumbers(a: Double, b: Double): Double {
-        return a + b
+    private var currentInput = ""
+    private var firstOperand = 0.0
+    private var currentOperator = ""
+
+    fun calculate(a: Double, b: Double, op: String): Double {
+        return when(op) {
+            "+" -> a + b
+            "-" -> a - b
+            "*" -> a * b
+            "/" -> if(b != 0.0) a / b else 0.0
+            else -> 0.0
+        }
     }
 }
