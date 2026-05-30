f51b58e9a9e8548231fc96e33df9686e7653bfd5
Vera Marie Therese Abayan Orbello <YOUR_GITHUB_EMAIL>
2026-05-30 12:52:45 +0800

    Task D1

--- Integrated/EchoApp/app/src/main/java/com/example/echoapp/CalculatorActivity.kt
+++ Integrated/EchoApp/app/src/main/java/com/example/echoapp/CalculatorActivity.kt
@@ -2,7 +2,13 @@ package com.example.echoapp
 
 class CalculatorActivity {
 
-    fun addNumbers(a: Double, b: Double): Double {
-        return a + b
+    fun calculate(a: Double, b: Double, op: String): Double {
+        return when(op) {
+            "+" -> a + b
+            "-" -> a - b
+            "*" -> a * b
+            "/" -> a / b
+            else -> 0.0
+        }
     }
 }
